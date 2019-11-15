a52d3f4f794d20a171d30f2b7be928f3a2d09d3c
Link <979192956@qq.com>
2019-11-15 20:00:58 +0800

    继承

---
+++ Practice/继承/继承/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  继承
+//
+//  Created by Link on 2019/11/13.
+//  Copyright © 2019 Link. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+class Person {
+    
+    var name: String
+    
+    //指定初始化器
+    init() {
+        self.name = "name"
+    }
+    //便捷初始化器
+    convenience init(name: String) {
+        self.init()
+    }
+}
+
+class Man: Person {
+    convenience init(name: String) {
+        self.init()
+    }
+}
