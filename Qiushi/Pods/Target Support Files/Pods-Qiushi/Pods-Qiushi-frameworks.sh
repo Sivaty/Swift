52d8acb0ca0269ffafe651f06376c3be3a42f2e3
Link <979192956@qq.com>
2019-12-18 21:41:47 +0800

    debug tool

--- Qiushi/Pods/Target Support Files/Pods-Qiushi/Pods-Qiushi-frameworks.sh
+++ Qiushi/Pods/Target Support Files/Pods-Qiushi/Pods-Qiushi-frameworks.sh
@@ -162,6 +162,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaDebug/CocoaDebug.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KakaJSON/KakaJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
