469d708b03a23504b7282bec759151328ff6dd05
Link <sengshuaibin@vcomic.com>
2019-12-19 09:56:53 +0800

    custom view

---
+++ CustomCollectionView-Swift/Pods/Target Support Files/Pods-CustomCollectionView-Swift-CustomCollectionView-SwiftUITests/Pods-CustomCollectionView-Swift-CustomCollectionView-SwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomCollectionView_Swift_CustomCollectionView_SwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CustomCollectionView_Swift_CustomCollectionView_SwiftUITests
+@end
---
+++ CustomCollectionView-Swift/Pods/Target Support Files/Pods-CustomCollectionView-SwiftTests/Pods-CustomCollectionView-SwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomCollectionView_SwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_CustomCollectionView_SwiftTests
+@end
